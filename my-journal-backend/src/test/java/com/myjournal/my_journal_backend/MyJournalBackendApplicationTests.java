3934ce418279e16b8cad1b8c839ec9c01947a74b
ayly8 <allison.y.ly@gmail.com>
2025-02-22 14:43:11 -0800

    setup java spring root for backend

---
+++ my-journal-backend/src/test/java/com/myjournal/my_journal_backend/MyJournalBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myjournal.my_journal_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyJournalBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
